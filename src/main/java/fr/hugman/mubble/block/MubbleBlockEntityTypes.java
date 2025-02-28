b36dce73bff481c02a9f63849e6dcec254e95283
maxbrick <maximusbrick@gmail.com>
2025-02-28 14:17:54 -0500

    removed deprecated function

--- src/main/java/fr/hugman/mubble/block/MubbleBlockEntityTypes.java
+++ src/main/java/fr/hugman/mubble/block/MubbleBlockEntityTypes.java
@@ -1,16 +1,13 @@
 package fr.hugman.mubble.block;
 
-import com.mojang.datafixers.types.Type;
 import fr.hugman.mubble.Mubble;
 import fr.hugman.mubble.block.entity.BumpableBlockEntity;
 import fr.hugman.mubble.block.entity.WarpBlockEntity;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.datafixer.TypeReferences;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.util.Util;
 
 public class MubbleBlockEntityTypes {
     public static final BlockEntityType<BumpableBlockEntity> BUMPABLE_BLOCK = of("bumpable_block", FabricBlockEntityTypeBuilder.create(BumpableBlockEntity::new,
@@ -43,7 +40,6 @@ public class MubbleBlockEntityTypes {
     ));
 
     private static <T extends BlockEntity> BlockEntityType<T> of(String path, FabricBlockEntityTypeBuilder<T> blockEntityType) {
-        Type<?> type = Util.getChoiceType(TypeReferences.BLOCK_ENTITY, path);
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Mubble.id(path), blockEntityType.build(type));
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Mubble.id(path), blockEntityType.build());
     }
 }
